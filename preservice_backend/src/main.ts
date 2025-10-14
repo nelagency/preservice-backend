1f879ffb278a20a105b34cde8bacb5a76e842904
Dechateau2035 <dechateaufoyou@gmail.com>
2025-10-15 00:44:15 +0100

    Update BackEnd1.0

--- preservice_backend/src/main.ts
+++ preservice_backend/src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 
