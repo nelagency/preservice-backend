44196bf4c9defcc5fe486a51031f292f8188e689
Dechateau2035 <dechateaufoyou@gmail.com>
2025-10-29 10:10:47 +0100

    LastOne else

--- preservice_backend/src/main.ts
+++ preservice_backend/src/main.ts
@@ -28,8 +28,7 @@ async function bootstrap() {
     'http://localhost:3001',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
-    'https://prest-service-front-9fm7.vercel.app', 
-    'https://prest-service-front-one.vercel.app'
+    'https://prest-service-front-ashen.vercel.app'
   ]);
 
   app.enableCors({
